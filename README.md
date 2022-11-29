039d8cfd9e3e2a17252353cd5f13f8d8d7638789
Mike Kovacina <1504893+mkovacina@users.noreply.github.com>
2022-11-28 21:18:21 -0500

    Class notes for 2022-11-17 (#2)

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
 - [VS Code](https://code.visualstudio.com/)
 - [Visual Studio](https://visualstudio.microsoft.com/vs/community/)
 
+
 - [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture)
 - [3n+1 ACM Contest Practice Problem](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=36)
 - [sequence reference for collatz](https://oeis.org/A006577)
+
+
+- [Map/Reduce](https://en.wikipedia.org/wiki/MapReduce)
+
+
+- [knuth](https://en.wikipedia.org/wiki/Donald_Knuth)
--- main/src/App.java
+++ main/src/App.java
@@ -1,8 +1,39 @@
 import java.time.Duration;
 import java.time.Instant;
 
+import javax.xml.stream.events.StartDocument;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        // the current in milliseconds
+        var startOld = System.currentTimeMillis();
+        var endOld = System.currentTimeMillis();
+        var dureationOld = endOld - startOld;
+
+        var iterativeMethod = new IterativeHailstoneMethod();
+        var iterativeDuration = Measure(iterativeMethod);
+
+        var recursiveMethod = new RecursiveHailstoneMethod();
+        var recursiveDuration = Measure(recursiveMethod);
+
+        System.out.println("Recursive duration = "+recursiveDuration.toMillis());
+        System.out.println("Iterative duration = "+iterativeDuration.toMillis());
+    }
+
+    public static Duration Measure(HailstoneMethod method)
+    {
+        var start = Instant.now();
+
+        for(int x = 0; x < 1000000; x++)
+        {
+            var trajectory = method.ComputeTrajectory(314159);
+        }
+
+        var end = Instant.now();
+
+        var duration = Duration.between(start, end);
+
+        return duration;
     }
 }
 
